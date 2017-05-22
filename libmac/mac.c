fb3b622bbc7d5f3c0da40f044c974d09ef4ee70b
Yoshisato Yanagisawa <yyanagisawa@google.com>
2017-05-22 17:34:40 +0900

    Workaround on sigset_t size difference between Linux and macOS.
    Since the original implementation did not do ABI conversion, I guess it
    might not work as expected.  Moreover, it should cause buffer overflow
    since sizeof(sigset_t) is 1024 bits on Linux while it is 32bits on macOS.
    
    Since nobody noticed the issue until now, there might not be the program
    running on maloader that requires sigprocmask and sigmask.  Let me make
    them do nothing but returning 0 (success).
    Also, not to cause buffer overflow on sigfillset, which could be called
    before sigprocmask, let me emulate the function in libmac.

--- libmac/mac.c
+++ libmac/mac.c
@@ -81,6 +81,7 @@ typedef uint16_t __darwin_nlink_t;
 typedef uint64_t __darwin_ino64_t;
 typedef uint32_t __darwin_uid_t;
 typedef uint32_t __darwin_gid_t;
+typedef uint32_t __darwin_sigset_t;
 
 // From /usr/include/sys/stat.h
 struct __darwin_stat64 {
@@ -1219,29 +1220,19 @@ void* __darwin_signal() {
   return NULL;
 }
 
-#define __DARWIN_SIG_BLOCK 1
-#define __DARWIN_SIG_UNBLOCK 2
-#define __DARWIN_SIG_SETMASK 3
-static int __translate_sigprocmask_how(int how) {
-  switch (how) {
-  case __DARWIN_SIG_BLOCK:
-    return SIG_BLOCK;
-  case __DARWIN_SIG_UNBLOCK:
-    return SIG_UNBLOCK;
-  case __DARWIN_SIG_SETMASK:
-    return SIG_SETMASK;
-  default:
-    fprintf(stderr, "Unknown sigprocmask how: %d\n", how);
-    return how;
-  }
+int __darwin_sigprocmask(
+    int how, const __darwin_sigset_t *set, __darwin_sigset_t *oldset) {
+  return 0;
 }
 
-int __darwin_sigprocmask(int how, const sigset_t *set, sigset_t *oldset) {
-  return sigprocmask(__translate_sigprocmask_how(how), set, oldset);
+int __darwin_pthread_sigmask(
+    int how, const __darwin_sigset_t *set, __darwin_sigset_t *oldset) {
+  return 0;
 }
 
-int __darwin_pthread_sigmask(int how, const sigset_t *set, sigset_t *oldset) {
-  return pthread_sigmask(__translate_sigprocmask_how(how), set, oldset);
+int __darwin_sigfillset(__darwin_sigset_t *set) {
+  *set = ~0UL;
+  return 0;
 }
 
 typedef struct malloc_statistics_t {
